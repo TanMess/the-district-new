738ee974538b687feda60fd04b191fa21aea5bc7
TanMess <breant121@gmail.com>
2024-08-28 11:08:54 +0200

    pagination

--- src/Controller/FlatController.php
+++ src/Controller/FlatController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\FlatRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FlatController extends AbstractController
 {
     #[Route('/flat', name: 'app_flat')]
-    public function index(FlatRepository $repository): Response
+    public function index(FlatRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        $flats = $repository->findAll();
+        $flats = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), 
+            10 
+        );
         return $this->render('pages/flat/index.html.twig', [
             'flats' => $flats
         ]);
