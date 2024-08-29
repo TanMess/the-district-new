a23f36878c4e6f62d6ef899194b09170c789a837
TanMess <breant121@gmail.com>
2024-08-29 09:53:58 +0200

    FileType

--- src/Controller/FlatController.php
+++ src/Controller/FlatController.php
@@ -8,6 +8,7 @@ use App\Repository\FlatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,33 +20,33 @@ class FlatController extends AbstractController
     {
         $flats = $paginator->paginate(
             $repository->findAll(),
-            $request->query->getInt('page', 1), 
-            10 
+            $request->query->getInt('page', 1),
+            10
         );
         return $this->render('pages/flat/index.html.twig', [
             'flats' => $flats
         ]);
     }
     #[Route('/plat/nouveau', name: 'app_flat', methods: ['GET', 'POST'])]
-    public function new( Request $request, EntityManagerInterface $entityManager): Response{
+    public function new(Request $request, EntityManagerInterface $entityManager, #[Autowire('%photo_dir%')] string $photodir): Response
+    {
 
         $flat = new Flat();
         $form = $this->createForm(FlatType::class, $flat);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $flat = $form->getData();
-            
-
+            if ($image = $form['image']->getData()) {
+                $filename = uniqid() . '.' . $image->guessExtension();
+                $image->move($photodir, $filename);
+            }
+          
             $entityManager->persist($flat);
             $entityManager->flush();
             return $this->redirectToRoute('app_flat');
         }
         return $this->render('pages/flat/new.html.twig', [
-            'form' =>$form->createView()
+            'form' => $form->createView()
         ]);
-
-
-
-
     }
 }
