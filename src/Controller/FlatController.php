d00568c4c92622efd784f4654b14077f93d340de
TanMess <breant121@gmail.com>
2024-08-29 08:21:44 +0200

    FlatType

--- src/Controller/FlatController.php
+++ src/Controller/FlatController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Flat;
 use App\Form\FlatType;
 use App\Repository\FlatRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,11 +26,20 @@ class FlatController extends AbstractController
             'flats' => $flats
         ]);
     }
-    #[Route('/plat/nouveau', 'plat.new', methods: ['GET', 'POST'])]
-    public function new(): Response{
+    #[Route('/plat/nouveau', name: 'app_flat', methods: ['GET', 'POST'])]
+    public function new( Request $request, EntityManagerInterface $entityManager): Response{
 
         $flat = new Flat();
         $form = $this->createForm(FlatType::class, $flat);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $flat = $form->getData();
+            
+
+            $entityManager->persist($flat);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_flat');
+        }
         return $this->render('pages/flat/new.html.twig', [
             'form' =>$form->createView()
         ]);
--- src/Form/FlatType.php
+++ src/Form/FlatType.php
@@ -4,19 +4,89 @@ namespace App\Form;
 
 use App\Entity\Flat;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Image;
 
 class FlatType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('description')
-            ->add('image')
-            ->add('active')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50'                
+                ],
+                'label' => 'Nom',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',             
+                ],
+                'label' => 'Prix',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    new Assert\LessThan(200)
+                ]
+
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',               
+                ],
+                'label' => 'Description',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                'required' => false,
+                'mapped' => false
+                ],
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' =>[
+                    new Image (['maxSize' => '5000k'])
+                ]
+            ])
+            ->add('active', RadioType::class, [
+                
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Creer mon plat'
+            ])
         ;
     }
 
