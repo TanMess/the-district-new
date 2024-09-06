85df6ab4612deb67e32d8fcaf9838703159c6c57
TanMess <breant121@gmail.com>
2024-09-06 09:42:28 +0200

    ContactForm

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contact', name: 'contact.index')]
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre demande a bien été envoyé !'
+            );
+            return $this->redirectToRoute('contact.index');
+        }
+        return $this->render('pages/contact/index.html.twig', [
+            'form' => $form->createView()
         ]);
     }
-}
+        }
+
+        
+
