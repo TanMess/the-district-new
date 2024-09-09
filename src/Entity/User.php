1196d87ad35ed69864ae139b15b8ac56e3e4603f
TanMess <breant121@gmail.com>
2024-09-09 15:07:18 +0200

    Registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,7 +49,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     #[Assert\NotBlank()]
-    private ?string $password = null;
+    private ?string $password = 'password';
 
     
 
