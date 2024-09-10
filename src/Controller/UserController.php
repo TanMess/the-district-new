f578c0afb974b4d37f9a2566514d58ed3ce2663c
TanMess <breant121@gmail.com>
2024-09-10 12:54:34 +0200

    EditUser

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,25 +4,50 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/utilisateur/edition/{id}', name: 'user.edit')]
-    public function edit(User $user): Response
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
+    public function edit(User $user, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
     {
 
-        if (!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('security.login');
         }
 
-        if($this->getUser() === $user) {
+        if ($this->getUser() !== $user) {
             return $this->redirectToRoute('flat.index');
         }
 
         $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()->getPlainPassword())) {
+                $user = $form->getData();
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Les informations de votre compte ont bien été modifiées !'
+                );
+
+                return $this->redirectToRoute('flat.index');
+            } else{
+                $this->addFlash(
+                'warning',
+                'Le mot de passe renseigné est incorrect !'
+            );
+        }
+    }
+
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -49,25 +48,7 @@ class UserType extends AbstractType
                     new Assert\Length(['min' => 2, 'max' => 50]),
                 ]
             ])
-            ->add('email', EmailType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '180'                
-                ],
-                'label' => 'Adresse email',
-                'label_attr' =>[
-                    'class' => 'form-label mt-4'
-                ],
-                'constraints' => [
-                    new Assert\Length(['min' => 2, 'max' => 180]),
-                    new Assert\NotBlank(),
-                    new Assert\Email()
-                ]
-            ])
-            ->add('PlainPassword', RepeatedType::class, [
-                'type' =>PasswordType::class,
-                'first_options' => [
+            ->add('PlainPassword', PasswordType::class, [
                     'attr' => [
                         'class' => 'form-control'
                     ],
@@ -75,25 +56,13 @@ class UserType extends AbstractType
                     'label_attr' => [
                         'class' => 'form-label mt-4'
                     ]
-                ],
-                'second_options' => [
-                    'attr' => [
-                        'class' => 'form-control'
-                    ],
-                    'label' => 'Confirmation du mot de passe',
-                    'label_attr' => [
-                        'class' => 'form-label mt-4'
-                    ]
-                ],
-                'invalid_message' => 'Les mots de passe ne correspondent pas'
+                
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
                 ],
             ]);
-        
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
